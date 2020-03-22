7e3eb0c2b3c33716f9361d34a7de7d266a39da8d
Lukasz Kierepka <lukasz_kierepka@hotmail.com>
2020-03-22 08:01:00 +0100

    Improving best selection performance

--- src/main/java/selection/BestSelection.java
+++ src/main/java/selection/BestSelection.java
@@ -2,6 +2,7 @@ package selection;
 
 import model.Individual;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -17,26 +18,12 @@ public class BestSelection extends SelectionMethod {
 
     @Override
     public List<Individual> select(List<Individual> individuals, int newPopulationSize) {
-        LinkedList<Individual> newPopulation = new LinkedList<Individual>();
-        LinkedList<Individual> theBestPopulation = new LinkedList<Individual>();
         int amountOfBests = (int) (individuals.size() / percentageOfBest);
+        Collections.sort(individuals);
+        LinkedList<Individual> elites = new LinkedList<>();
         for (int i = 0; i < amountOfBests; i++) {
-            Individual theBestOfPopulation = selectTheBest(individuals);
-            individuals.remove(theBestOfPopulation);
-            theBestPopulation.add(theBestOfPopulation);
+            elites.add(individuals.get(i));
         }
-        for (int i = 0; i < newPopulationSize; i++) {
-            newPopulation.add(theBestPopulation.get(i % amountOfBests));
-        }
-        return newPopulation;
-    }
-
-    private Individual selectTheBest(List<Individual> individuals) {
-        Individual theBestIndividual = individuals.get(0);
-        for (Individual individual : individuals) {
-            if (theBestIndividual.getY() < individual.getY())
-                theBestIndividual = individual;
-        }
-        return theBestIndividual;
+        return elites;
     }
 }
